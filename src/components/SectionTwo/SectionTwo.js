ba71f6d37bba929703d1c3fd455b6dccd06ac6af
Mayk Gomes <maykcontato98@hotmail.com>
2022-08-12 16:36:57 -0300

    .

--- src/components/SectionTwo/SectionTwo.js
+++ src/components/SectionTwo/SectionTwo.js
@@ -8,6 +8,8 @@ import projeto3 from '../../assets/projeto3.jpg'
 import projeto4 from '../../assets/projeto4.jpg'
 import projeto5 from '../../assets/projeto5.jpg'
 import projeto6 from '../../assets/projeto6.jpg'
+import projeto7 from '../../assets/projeto7.jpg'
+import emConstru from '../../assets/emConstru.png'
 import PaineisProject from './PaineisProject/PaineisProject'
 import ButtonNav from '../ButtonNav/ButtonNav'
 
@@ -109,15 +111,15 @@ const SectionTwo = () => {
             initial='variantA'
             animate='variantB'>
 
-            <PaineisProject linkProjeto='https://react-series.vercel.app/' linkGithub='https://github.com/MaykGomes92/React-Series' imgProject={projeto1} nameProject='Website Séries' descricao=' O projeto consiste em ser um site de séries completo, tendo todas as séries populares e existentes da api IMDb.' mLeft='228px' />
+            <PaineisProject html={html} react={react} css={css} js={js} linkProjeto='https://gerador-senha-private.vercel.app/' linkGithub='https://github.com/MaykGomes92/Gerador-de-Senha---React' imgProject={projeto7} nameProject='Gerador de Senha' descricao='Gerador de senha automatizado com 3 opções de caracteres, e podendo escolher quantos caracteres desejar.' mLeft='228px' />
           </motion.h3>
 
           <motion.h3
             variants={variantAnimationB}
             initial='variantA'
             animate='variantB'>
-            <PaineisProject linkProjeto='https://website-vikings-router-priv.vercel.app/'
-              linkGithub='https://github.com/MaykGomes92/WebSite-Vikings-ReactRouter' imgProject={projeto2} nameProject='Website Vikings' descricao='Projeto feito usando React Router e outras libs do React.' mLeft='500px' />
+            <PaineisProject linkProjeto='https://github.com/MaykGomes92'
+              linkGithub='https://github.com/MaykGomes92' imgProject={emConstru} nameProject='Em Construção' descricao='Novos projetos estão sendo feitos, irei postando os melhores aqui o/.' mLeft='500px' />
           </motion.h3>
         </>
       )
